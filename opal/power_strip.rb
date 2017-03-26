8d8fabd95008d356bd388bca1f7744afb1e02073
Jamie Gaskins <jgaskins@gmail.com>
2017-03-26 16:12:05 -0400

    Restructure event handling for client code
    Instead of channels knowing about raw sockets, they only know about the
    client. This should also make writing tests for the client easier.

--- opal/power_strip.rb
+++ opal/power_strip.rb
@@ -9,18 +9,36 @@ module PowerStrip
       @url = url
       @socket = Bowser.websocket(url)
       @socket.autoreconnect!
-      @subscriptions = []
+      @subscriptions = {}
 
-      @socket.on(:open) { @subscriptions.each(&:subscribe) }
+      @socket.on :open do
+        @subscriptions.each { |name, channel| channel.subscribe }
+      end
+      @socket.on :message do |event|
+        message = Message.new(event.data)
+        channel = @subscriptions[message.channel]
+
+        if channel
+          channel.receive_message message
+        end
+      end
     end
 
     def subscribe channel_name
-      channel = Channel.new(channel_name, @socket)
-      @subscriptions << channel
+      channel = Channel.new(channel_name, self)
 
-      if @socket.connected?
-        channel.subscribe
-      end
+      @subscriptions[channel_name] = channel
+      channel.subscribe
+
+      channel
+    end
+
+    def unsubscribe channel_name
+      channel = @subscriptions[channel_name]
+      return if channel.nil?
+
+      @subscriptions.delete channel
+      channel.unsubscribe
 
       channel
     end
@@ -38,45 +56,60 @@ module PowerStrip
 
       @socket.on native_event, &block
     end
-  end
 
-  Channel = Struct.new(:name, :socket) do
-    def on event_name, &block
-      socket.on :message do |event|
-        message = Message.new(event.data)
-
-        if message.channel == name && message.event == event_name
-          block.call message
-        end
-      end
-    end
-
-    def subscribe
-      socket.send_message(
-        event: :@subscribe,
-        channel: name,
-      )
-    end
-
-    def method_missing event, message
+    def send_message channel=nil, event=nil, data=nil
       promise = Promise.new
 
-      if socket.connected?
-        socket.send_message({
-          channel: name,
-          event: event,
-          data: message,
-        }.select { |k,v| v })
+      if @socket.connected?
+        @socket.send_message({ channel: channel, event: event, data: data }
+          .select { |k,v| v })
         promise.resolve true
       else
         Bowser.window.delay 1 do
-          send(event, message).then { promise.resolve true }
+          send_message(channel, event, data)
+            .then { promise.resolve true }
         end
       end
 
       promise
     rescue => exception
       promise.fail exception
+    end
+  end
+
+  class Channel
+    attr_reader :name, :client
+
+    def initialize name, client
+      @name = name
+      @client = client
+      @handlers = Hash.new { |h, k| h[k] = [] }
+    end
+
+    def on event_name, handler=nil, &block
+      unless handler || block
+        raise ArgumentError, 'PowerStrip::Channel#on requires an event handler'
+      end
+
+      @handlers[event_name] << (handler || block)
+    end
+
+    def receive_message message
+      @handlers[message.event].each do |handler|
+        handler.call message
+      end
+    end
+
+    def subscribe
+      send :@subscribe, nil
+    end
+
+    def unsubscribe
+      send :@unsubscribe, nil
+    end
+
+    def send event, message=nil
+      client.send_message(name, event, message)
     end
   end
 
